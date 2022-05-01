9e85ea48e1db6a2ef68f4a2e6cc527f28914aa2d
Jay Murphy (murphyj10) <murphyj10@coventry.ac.uk>
2022-05-01 13:53:02 +0100

    Added catcher for bad links

--- reader.py
+++ reader.py
@@ -1,4 +1,5 @@
 import csv
+from sys import api_version
 import constants as cons
 
 ## trn ID from overview - > Network -> Steam ID -> data -> metadata -> playerId
@@ -23,17 +24,23 @@ with open('trackers.csv') as csvfile: ## Opens Trackers input file
     for data in inputdataList: ## For every entry in data List ->
         linksList.append(data[2]) ## Add trackers links to links list
 
-    for links in linksList: ## For every link in link list ->
-        if 'https://rocketleague.tracker.network/rocket-league/profile/' in links:
-            ident = links.split('/', 5) ## Remove link info (Counts # of '/'), keep identifiers
+    for link in linksList: ## For every link in link list ->
+        if 'https://rocketleague.tracker.network/rocket-league/profile/' in link:
+            ident = link.split('/', 5) ## Remove link info (Counts # of '/'), keep identifiers
             ident = [ident.replace('/overview', '') for ident in ident] ## Remove '/overview' from links
             identList.append(ident[5]) ## Add identifiers to indentifier list
+    else:
+        if 'https://rocketleague.tracker.network/rocket-league/profile/' not in link:
+            identList.append("BAD-LINK")
+    
+    for ident in identList:
+        if ident != "BAD-LINK":
+            api_linksList.append(cons.PROF_ENDP + ident)
+        else:
+            api_linksList.append(ident)
 
 
 
-
-
-api_linksList = [cons.PROF_ENDP + profile for profile in identList] ## Add profile endpoint to profiles in list (PROF_ENDP/<PLATFORM>/<ID>)
 
 print(f"{len(api_linksList)} Entries in file")
 print("-------------------------------------")
