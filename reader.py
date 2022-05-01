72f2d4aa5a8572153277cbd5c9ac4a91b3507670
Jay Murphy (murphyj10) <murphyj10@coventry.ac.uk>
2022-05-01 13:41:42 +0100

    Removed redundant counter and list comprehension

--- reader.py
+++ reader.py
@@ -11,17 +11,14 @@ import constants as cons
 inputdataList = [] ## Start with empty list for RSC ID, Name, Tracker Link
 linksList = [] ## Start with empty list for tracker links
 identList = [] ## Start with empty list for platform & ident -> (platform/identifier)
-prof_linksList = [] ## Start with empty list for profile api links
+api_linksList = [] ## Start with empty list for profile api links
 
 
 with open('trackers.csv') as csvfile: ## Opens Trackers input file
-    count = 0 ## Set count to track number of entries
-    trackers = csv.reader(csvfile) ## Defines file as 'trackers'
+    trackers = csv.reader(csvfile) 
 
     for row in trackers: ## For every row in trackers ->
         inputdataList.append(row[0:3]) ## Add to data list -> [0]RSC ID,[1]Name,[2]Link ([start:end-1]) 
-        count += 1 ## Add 1 to count
-    print(str(count) + ' ENTRIES IN FILE \n')
 
     for data in inputdataList: ## For every entry in data List ->
         linksList.append(data[2]) ## Add trackers links to links list
@@ -31,15 +28,15 @@ with open('trackers.csv') as csvfile: ## Opens Trackers input file
             ident = links.split('/', 5) ## Remove link info (Counts # of '/'), keep identifiers
             ident = [ident.replace('/overview', '') for ident in ident] ## Remove '/overview' from links
             identList.append(ident[5]) ## Add identifiers to indentifier list
-        else:
-            identList.append('iJxG.?')
 
 
 
 
 
-appended_links = [cons.PROF_ENDP + profile for profile in identList] ## Add profile endpoint to profiles in list (PROF_ENDP/<PLATFORM>/<ID>)
+api_linksList = [cons.PROF_ENDP + profile for profile in identList] ## Add profile endpoint to profiles in list (PROF_ENDP/<PLATFORM>/<ID>)
 
-for entry in appended_links: ## For every entry in appended_links ->
-    prof_linksList.append(entry) ## Add entry to prof_linksList
-
+print(f"{len(api_linksList)} Entries in file")
+print("-------------------------------------")
+for link in api_linksList:
+    print(link)
+    
