0aad65e8bd06fa146db2d0ada660141007cc36de
Jay Murphy (murphyj10) <murphyj10@coventry.ac.uk>
2022-05-17 01:33:09 +0100

    Removed redundant line for headers

--- scraper.py
+++ scraper.py
@@ -144,7 +144,8 @@ async def pullMMR(profEndPointList):
     with open("output.csv", 'w', newline="") as output:
         headerList = ['RSC ID', 'Name', 'Link', 'Peak 2s', 'Peak 3s']
         writeCount = 1
-        write = csv.writer(output, headerList)
+        write = csv.writer(output)
+        write.writerow(headerList)
         for set in finishedList:
             print(f'Writing MMRs for entry {writeCount} of {len(finishedList)}')
             write.writerow(set)
